57a0c1f33792bbe69f1587e11ca403e2bfb44856
Martín Batista <martdev1963@gmail.com>
2018-07-17 14:57:38 -0400

    GUI program 1st draft
    GUI program that converts Kg user input to grams, pounds and ounces.

--- script1.py
+++ script1.py
@@ -7,24 +7,68 @@ from tkinter import *
 window = Tk() # this works too.
 
 # function that converts km to mi...
-def km_to_miles():
+#def km_to_miles():
     #print(e1_value.get()) # the get() method of the StringVar object...
-    miles=float(e1_value.get())*1.6
-    t1.insert(END,miles) # place this value after the last entry...
+#    miles=float(e1_value.get())*1.6
+#    t1.insert(END,miles) # place this value after the last entry...
+
+# FUNCTION DEFINITION...
+# function that converts from metric to SAE standard...
+def kilogram_to_grams_pounds_ounces():
+    # conversion formulae...
+    grams=float(kilogram_entry_value.get())*1000
+    pounds=float(kilogram_entry_value.get())*2.20462
+    ounces=float(kilogram_entry_value.get())*35.274
+
+    grams_display.delete("1.0", END)
+    grams_display.insert(END,grams)
+    pounds_display.delete("1.0", END)
+    pounds_display.insert(END,pounds)
+    ounces_display.delete("1.0", END)
+    ounces_display.insert(END,ounces)
 
 # widgets (widgets go within the main window)
-b1=Button(window,text="Execute",command=km_to_miles) # parameters, separated by commas...You just pass the function name without the function()
+#b1=Button(window,text="Execute",command=km_to_miles) # parameters, separated by commas...You just pass the function name without the function()
+
+# BUTTON WIDGET... clicking the button will invoke function callback...
+b1=Button(window,text="Execute",command=kilogram_to_grams_pounds_ounces) # parameters, separated by commas...You just pass the function name without the function()
 #b1.pack()
 # alternative way for .pack()
-b1.grid(row=0,column=0)
 
-e1_value=StringVar() # instantiate a StringVar object...
-e1=Entry(window,textvariable=e1_value) # e1_value for the user input...
-e1.grid(row=0,column=1)
+# button coordinate location on window object...
+b1.grid(row=1,column=0)
 
-t1=Text(window,height=1,width=20)
-t1.grid(row=0,column=2)
 
+# instantiate StringVar object for user entry...
+kilogram_entry_value=StringVar()
+
+
+# ENTRY WIDGET...
+
+# label for entry user input widget...though its a separate widget for the label alone.
+e_label=Label(window,text="Kg")
+e_label.grid(row=0,column=0)
+
+entry_kilogram=Entry(window,textvariable=kilogram_entry_value) # passing StringVar object so it can be displayed as user input...
+entry_kilogram.grid(row=0,column=1) # entry widget coordinate....
+
+#e1_value=StringVar() # instantiate a StringVar object...
+#e1=Entry(window,textvariable=e1_value) # e1_value for the user input...
+#e1.grid(row=0,column=1)
+
+# OUTPUT WIDGETS...and their coordinates on the window object...
+# this displays the output result in the text field widget t1
+#t1=Text(window,height=1,width=20)
+#t1.grid(row=0,column=2)
+
+grams_display=Text(window,height=1,width=20)
+grams_display.grid(row=0,column=2)
+
+pounds_display=Text(window,height=1,width=20)
+pounds_display.grid(row=0,column=3)
+
+ounces_display=Text(window,height=1,width=20)
+ounces_display.grid(row=0,column=4)
 
 
 
